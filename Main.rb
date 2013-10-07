a08cb2b5f4891528ee1a1f237213300fc7b3726a
ccard <ccard@mymail.mines.edu>
2013-10-07 16:30:35 -0600

    started trying to get a login gui going

--- Main.rb
+++ Main.rb
@@ -1,5 +1,6 @@
 require_relative 'MainGui'
 require_relative 'emailClient'
+require_relative 'loginGui'
 require 'rubygems'
 require 'highline/import'
 
@@ -8,9 +9,16 @@ def get_pass(prompt="Enter Password")
 end
 pass = get_pass
 
+login = LoginGui.new "sukhoiscard@aol.com"
+
+until login.ready
+
+end
+
+puts login.getPass
 #mainframe = MainGui.new "EmailClient"
 
-email = EmailClient::EmailClient.new "sukhoiscard@aol.com",pass
+#email = EmailClient::EmailClient.new "sukhoiscard@aol.com",pass
 
-email.startSession
-email.sendMessage "sukhoiscard@aol.com","test","me"
+#email.startSession
+#email.sendMessage "sukhoiscard@aol.com","test","me"
